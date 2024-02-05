7c9edd612de7e56fea6727cef154bcb01da97781
patrick <patrick@ms.touk.io>
2024-02-05 15:55:45 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { connect } from './config/conn';
 import { init } from './config/init';
+import cors from 'cors';
 
 // Iniciar conexão
 const pool = connect()
 // Iniciar app express
 const app = init()
 const PORT = 3000;
+
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
